06268ac45ba6d0b9459ed4243cc1d1070b50981b
RDyego <dyego@alu.ufc.br>
2020-08-08 19:38:37 -0300

    Fix migrations

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -3,10 +3,8 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
     return knex.schema.createTable('connections', table => {
         table.increments('id').primary();
-        table.integer('week_day').notNullable();
-        table.integer('from').notNullable();
-        table.integer('to').notNullable();
-        table.integer('class_id').notNullable().references('id').inTable('class').onUpdate('CASCADE').onDelete('CASCADE');
+        table.timestamp('create_at').defaultTo('now()').notNullable();
+        table.integer('user_id').notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     });
 }
 
